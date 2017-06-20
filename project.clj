f83b0bb0000dd45ca64f2559baf9562c9c9d99b5
James Conroy-Finn <git@invetica.co.uk>
2017-06-20 20:30:31 +0100

    Move into invetica org

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring/ring-spec "0.0.2"
+(defproject invetica/ring-spec "0.0.2"
   :description "Clojure specs for Ring"
   :url "https://github.com/ring-clojure/ring-spec"
   :license {:name "The MIT License"
